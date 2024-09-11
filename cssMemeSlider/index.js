4576cd90a7bff02095be5bc9722d3f731ea031c8
kb_germ <k.b.germ1806@gmail.com>
2024-09-11 21:17:32 +0300

    feat: html css (Wed, Sep 11, 2024 09:17:32 PM)

---
+++ cssMemeSlider/index.js
@@ -0,0 +1,23 @@
+const memes = [
+  {
+    "img": "./memesCollection/firstMeme.jpg",
+    "text": "It happend to everyone..."
+  },
+  {
+    "img": "./memesCollection/secondMeme.jpg",
+    "text": "Best job for me!"
+  },
+  {
+    "img": "./memesCollection/thirdMeme.jpg",
+    "text": "Typical everyday life"
+  },
+  {
+    "img": "./memesCollection/fourthMeme.jpg",
+    "text": "Grandfather decided to share"
+  },
+  {
+    "img": "./memesCollection/fifthMeme.jpg",
+    "text": "Not funny meme("
+  }
+];
+
