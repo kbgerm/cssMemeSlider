ecdb55acec019afc3a7b16ead863851d819b2bf4
kb_germ <k.b.germ1806@gmail.com>
2024-09-12 17:35:56 +0300

    feat: handlers (Thu, Sep 12, 2024 05:35:56 PM)

--- cssMemeSlider/index.js
+++ cssMemeSlider/index.js
@@ -27,8 +27,14 @@ const memes = [
 ];
 
 let firstButton = document.querySelector('.first');
+let secondButton = document.querySelector('.second');
+let thirdButton = document.querySelector('.third');
+let fourthButton = document.querySelector('.fourth');
+let fifthButton = document.querySelector('.fifth');
 let slideContainer = document.querySelector('.slide-container');
 let textContainer = document.querySelector('.textContainer');
+let currentSlideImg;
+let currentSlideText;
 
 function findMeme(numeral) {
   let meme = memes.find(function isFirst(memeName) {
@@ -37,13 +43,20 @@ function findMeme(numeral) {
   return meme;
 }
 
-function makeSlide(meme) {
+function makeSlideImg(meme) {
   let newSlide = document.createElement("div");
   newSlide.className = "slidePicture";
 
   let slideImg = document.createElement("img");
   slideImg.src = meme.img;
 
+  newSlide.appendChild(slideImg);
+  slideContainer.append(newSlide);
+  
+  return newSlide;
+}
+
+function makeSlideText(meme) {
   let newText = document.createElement("div");
   newText.className = "slideText";
 
@@ -51,16 +64,105 @@ function makeSlide(meme) {
   text.className = ".slideText p";
   text.textContent = meme.text;
 
-  newSlide.appendChild(slideImg);
   newText.appendChild(text);
-  slideContainer.append(newSlide);
   textContainer.append(newText);
 
-  return newSlide, newText;
+  return newText;
 }
 
 
 document.addEventListener('DOMContentLoaded', () => {
   firstButton.classList.add('active');
-  makeSlide(findMeme('first'));
+  currentSlideImg = makeSlideImg(findMeme('first'));
+  currentSlideText = makeSlideText(findMeme('first'));
+
+  // return [currentSlideText, currentSlideImg];
 });
+
+firstButton.addEventListener('click', firstButtonHandler);
+
+function firstButtonHandler() {
+  thirdButton.classList.remove('active');
+  secondButton.classList.remove('active');
+  firstButton.classList.add('active');
+  fourthButton.classList.remove('active');
+  fifthButton.classList.remove('active');
+
+  currentSlideText.classList.add('prev');
+  currentSlideImg.classList.add('prev');
+
+  currentSlideImg.remove();
+  currentSlideText.remove();
+
+  currentSlideImg = makeSlideImg(findMeme('first'));
+  currentSlideText = makeSlideText(findMeme('first'));
+}
+
+secondButton.addEventListener('click', secondButtonHandler);
+
+function secondButtonHandler() {
+  firstButton.classList.remove('active');
+  secondButton.classList.add('active');
+
+  currentSlideText.classList.add('prev');
+  currentSlideImg.classList.add('prev');
+
+  currentSlideImg.remove();
+  currentSlideText.remove();
+
+  currentSlideImg = makeSlideImg(findMeme('second'));
+  currentSlideText = makeSlideText(findMeme('second'));
+}
+
+thirdButton.addEventListener('click', thirdButtonHandler);
+
+function thirdButtonHandler() {
+  secondButton.classList.remove('active');
+  thirdButton.classList.add('active');
+
+  currentSlideText.classList.add('prev');
+  currentSlideImg.classList.add('prev');
+
+  currentSlideImg.remove();
+  currentSlideText.remove();
+
+  currentSlideImg = makeSlideImg(findMeme('third'));
+  currentSlideText = makeSlideText(findMeme('third'));
+}
+
+fourthButton.addEventListener('click', fourthButtonHandler);
+
+function fourthButtonHandler() {
+  thirdButton.classList.remove('active');
+  secondButton.classList.remove('active');
+  firstButton.classList.remove('active');
+  fourthButton.classList.add('active');
+
+  currentSlideText.classList.add('prev');
+  currentSlideImg.classList.add('prev');
+
+  currentSlideImg.remove();
+  currentSlideText.remove();
+
+  currentSlideImg = makeSlideImg(findMeme('fourth'));
+  currentSlideText = makeSlideText(findMeme('fourth'));
+}
+
+fifthButton.addEventListener('click', fifthButtonHandler);
+
+function fifthButtonHandler() {
+  thirdButton.classList.remove('active');
+  secondButton.classList.remove('active');
+  firstButton.classList.remove('active');
+  fourthButton.classList.remove('active');
+  fifthButton.classList.add('active');
+
+  currentSlideText.classList.add('prev');
+  currentSlideImg.classList.add('prev');
+
+  currentSlideImg.remove();
+  currentSlideText.remove();
+
+  currentSlideImg = makeSlideImg(findMeme('fifth'));
+  currentSlideText = makeSlideText(findMeme('fifth'));
+}
